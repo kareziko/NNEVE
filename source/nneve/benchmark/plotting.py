07d1919f3029db3cfb67ee6f05645ac0321107e4
Krzysztof Wiśniewski <argmaster.world@gmail.com>
2022-05-12 10:29:29 +0200

    Fix plotting issues, refactor cq

--- source/nneve/benchmark/plotting.py
+++ source/nneve/benchmark/plotting.py
@@ -8,10 +8,36 @@ from matplotlib.figure import Figure
 from numpy.typing import ArrayLike, NDArray
 from PIL import Image
 
+DEFAULT_COLOR_SET: Tuple[str, ...] = (
+    "#a83240",
+    "#5353f5",
+    "#0bb836",
+    "#c0d61c",
+    "#8339bf",
+    "#f56342",
+    "#4098e6",
+    "#e640a9",
+    "#70e630",
+)
+
 
 def datetag(
     date: Optional[datetime] = None, fmt: str = "%d_%m_%Y_%H_%M_%S_%f"
-):
+) -> str:
+    """Get formatted timestamp (eg. for file name)
+
+    Parameters
+    ----------
+    date : Optional[datetime], optional
+        datetime to format, by default None, meaning datetime.now()
+    fmt : str, optional
+        format to use, any compatible with datetime.strftime(), by default "%d_%m_%Y_%H_%M_%S_%f"
+
+    Returns
+    -------
+    str
+        String with date formatted according to fmt param.
+    """
     if date is None:
         date = datetime.now()
     return date.strftime(fmt)
@@ -31,6 +57,38 @@ def plot_multi_sample(  # noqa: CFQ002 CCR001
     fig: Optional[Figure] = None,
     ax: Optional[plt.Axes] = None,
 ) -> Tuple[Figure, plt.Axes]:
+    """Plot multiple sample ranges on same plot.
+
+    Parameters
+    ----------
+    x_range : Optional[ArrayLike], optional
+        optional sequence of x labels, do not have to resemble number of x values in samples, by default None
+    labels : Optional[Tuple[str, ...]], optional
+        Sample labels in legend, by default None, thus "Sample_{i}"
+    colors : Optional[Tuple[str, ...]], optional
+        Sequence of colors for each sample, by default None
+    x_axis_label : str, optional
+        String label for X axis, by default "Sample index"
+    y_axis_label : str, optional
+        String label for Y axis, by default "Execution time [s]"
+    linewidth : int, optional
+        Width of plot line, does not affect stats, by default 1
+    linestyle : str, optional
+        Style of plot line, does not affect stats, by default "-"
+    figsize : Tuple[int, int], optional
+        Size of figure, by default (14, 7)
+    add_stats : bool, optional
+        Wheather to include mean, median, min, max lines for each sample, by default False
+    fig : Optional[Figure], optional
+        Custom Figure, if None, new one is created, by default None
+    ax : Optional[plt.Axes], optional
+        Custom Axes, if None, new one is created, by default None
+
+    Returns
+    -------
+    Tuple[Figure, plt.Axes]
+        Figure and Axes used for drawing, usefull if any of them was generated in this function.
+    """
     if fig is None:
         fig = plt.figure(figsize=figsize)
 
@@ -38,24 +96,16 @@ def plot_multi_sample(  # noqa: CFQ002 CCR001
         ax = plt.axes()
 
     # find max size to ensure lines are long from left to right
-    n = max(len(cast(Sized, s)) for s in chain(samples, [()]))
+    n = _max_sample_size(samples)
 
     if labels is None:
-        labels = tuple(f"Sample {i}" for i in range(len(samples)))
+        labels = _default_sample_labels(len(samples))
 
     if colors is None:
-        colors = (
-            "#a83240",
-            "#5353f5",
-            "#0bb836",
-            "#c0d61c",
-            "#8339bf",
-            "#f56342",
-            "#4098e6",
-            "#e640a9",
-            "#70e630",
-        )
+        colors = DEFAULT_COLOR_SET
+
     x = np.arange(n)
+
     for sample, color, label in zip(samples, colors, labels):
         ax.plot(
             x,
@@ -66,16 +116,13 @@ def plot_multi_sample(  # noqa: CFQ002 CCR001
             color=color,
         )
         if add_stats:
-            ax.axhline(np.max(sample), 0, n, color=color[:7] + "40")
-            ax.axhline(np.mean(sample), 0, n, color=color[:7] + "c0")
-            ax.axhline(np.median(sample), 0, n, color=color[:7] + "80")
-            ax.axhline(np.min(sample), 0, n, color=color[:7] + "40")
+            _plot_sample_stats(ax, n, sample, color)
 
     ax.grid(True)
     ax.set_xlabel(x_axis_label)
     ax.set_ylabel(y_axis_label)
 
-    x_range_length = len(cast(Sized, x_range))
+    x_range_length = 0 if x_range is None else len(cast(Sized, x_range))
 
     if x_range is not None:
         if len(x) == x_range_length:
@@ -84,7 +131,25 @@ def plot_multi_sample(  # noqa: CFQ002 CCR001
             ax.set_xticks(np.linspace(0, n, x_range_length), x_range)
 
     ax.legend(loc="upper right")
+
     return fig, ax
+
+
+def _plot_sample_stats(
+    ax: plt.Axes, n: int, sample: ArrayLike, color: str
+) -> None:
+    ax.axhline(np.max(sample), 0, n, color=color[:7] + "40")
+    ax.axhline(np.mean(sample), 0, n, color=color[:7] + "c0")
+    ax.axhline(np.median(sample), 0, n, color=color[:7] + "80")
+    ax.axhline(np.min(sample), 0, n, color=color[:7] + "40")
+
+
+def _default_sample_labels(n: int) -> Tuple[str, ...]:
+    return tuple(f"Sample {i}" for i in range(n))
+
+
+def _max_sample_size(samples: Tuple[ArrayLike, ...]) -> int:
+    return max(len(cast(Sized, s)) for s in chain(samples, [()]))
 
 
 def pretty_bytes(i: int) -> str:  # noqa: CFQ004
--- tests/test_benchmark/test_plotting.py
+++ tests/test_benchmark/test_plotting.py
@@ -2,7 +2,9 @@ from pathlib import Path
 from typing import Callable, Optional
 
 import numpy as np
+import pytest
 from matplotlib.figure import Figure
+from numpy.typing import NDArray
 from PIL import Image
 
 from nneve.benchmark import get_image_identity_fraction, plot_multi_sample
@@ -11,12 +13,24 @@ DIR = Path(__file__).parent
 DATA_DIR = DIR / "data"
 
 
+@pytest.fixture()
+def example_matrix_1():
+    return np.loadtxt(DATA_DIR / "example_1_matrix_1.gz")
+
+
+@pytest.fixture()
+def example_matrix_2():
+    return np.loadtxt(DATA_DIR / "example_1_matrix_2.gz")
+
+
 def test_plot_with_stats_two_samples_auto_range(
-    fig_to_image: Callable[[Optional[Figure], Optional[str]], Image.Image]
+    fig_to_image: Callable[[Optional[Figure], Optional[str]], Image.Image],
+    example_matrix_1: NDArray,
+    example_matrix_2: NDArray,
 ):
     fig, _ = plot_multi_sample(
-        np.loadtxt(DATA_DIR / "example_1_matrix_1.gz"),
-        np.loadtxt(DATA_DIR / "example_1_matrix_2.gz"),
+        example_matrix_1,
+        example_matrix_2,
     )
     assert (
         get_image_identity_fraction(
@@ -32,13 +46,16 @@ def generate_sample(fig, i):  # pragma: no cover
 
 
 def test_plot_with_stats_two_samples_manual_range(
-    fig_to_image: Callable[[Optional[Figure], Optional[str]], Image.Image]
+    fig_to_image: Callable[[Optional[Figure], Optional[str]], Image.Image],
+    example_matrix_1: NDArray,
+    example_matrix_2: NDArray,
 ):
     fig, _ = plot_multi_sample(
-        np.loadtxt(DATA_DIR / "example_1_matrix_1.gz"),
-        np.loadtxt(DATA_DIR / "example_1_matrix_2.gz"),
+        example_matrix_1,
+        example_matrix_2,
         x_range=[0 for _ in range(30)],
         x_axis_label="Sample size",
+        add_stats=True,
     )
     generate_sample(fig, 2)
     assert (
